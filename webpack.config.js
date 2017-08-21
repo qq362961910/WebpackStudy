a1b4be78a8adb1cbba22716aba80b4591667b670
Amen <362961910@qq.com>
2017-08-21 20:36:30 +0800

    html中嵌入javascript

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,38 @@ module.exports = {
     
     entry: {
         main: './src/script/main.js',
-        a: ['./src/script/a.js', './src/script/b.js']
+        a: ['./src/script/main.js', './src/script/a.js'], 
+        b: ['./src/script/main.js', './src/script/b.js'],
+        c: ['./src/script/main.js', './src/script/c.js']
     },
 
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name]-[chunkhash].js'
+        filename: 'js/[name]-[chunkhash].js',
+        publicPath: 'http://cdn.com/'
     },
 
     plugins: [
         new htmlWebpackPlugin({
-            filename: 'index-[hash].html',
+            filename: 'a.html',
             template: 'index.html',
-            inject: 'head'
+            inject: false,
+            title: 'this is a.html',
+            chunks: ['main', 'a']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'b.html',
+            template: 'index.html',
+            inject: false,
+            title: 'this is b.html',
+            chunks: ['main', 'b']
+        }),
+        new htmlWebpackPlugin({
+            filename: 'c.html',
+            template: 'index.html',
+            inject: false,
+            title: 'this is c.html',
+            excludeChunks: ['a', 'b']
         })
     ]
 

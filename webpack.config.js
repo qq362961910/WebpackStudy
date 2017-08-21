eb37b624cfb2a39852f553cbcb731d82174fd97e
Amen <362961910@qq.com>
2017-08-21 15:20:46 +0800

    npm run webpack打包项目

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    
+    entry: './src/script/main.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist/js'),
+        filename: 'bundle.js'
+    }
+
+
+}
